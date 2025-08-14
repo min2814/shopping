fbfb8b9ecfea108185f26593a3c42319abab468f
min2814 <127213059+min2814@users.noreply.github.com>
2025-08-14 11:58:43 +0900

    장바구니 담은 후 페이지 이동

--- shopping_cart.js
+++ shopping_cart.js
@@ -5,25 +5,30 @@ cart.forEach((product, index) => {
   console.log(product);
   const li = document.createElement('li');
   li.innerHTML = `
-        <img src="${product.image}" alt="${product.title}">
-        <div>
-          <div class="shopping__product_detail">
-            <div>
-              <span>${product.title}</span>
-              <i class="fa-solid fa-trash-can" data-index="${index}"></i>
-            </div>
-            <div>
-              <span>${product.category}</span>
-              <i class="fa-regular fa-heart"></i>
-            </div>
-          </div>
-          <div class="shopping__product_price">
-            <div>$${product.price}</div>
-            <div>1</div>
-          </div>
-        </div>  
-      `;
-  ul.appendChild(li);
+  <img src="${product.image}" alt="${product.title}">
+  <div>
+    <div class="shopping__product_detail">
+      <div class="row">
+        <span>${product.title}</span>
+        <i class="fa-solid fa-trash-can" data-index="${index}"></i>
+      </div>
+      <div class="row">
+        <span>${product.category}</span>
+        <i class="fa-regular fa-heart"></i>
+      </div>
+      <div class="row">
+        <span>색상 : ${product.color} / 사이즈 : ${product.size}</span>
+        <button class="increase-btn" data-index="${index}">+</button>
+      </div>
+    </div>
+    <div class="row shopping__product_price">
+      <div>$${product.price}</div>
+      <div class="quantity" data-index="${index}">1</div>
+    </div>
+  </div>
+`;
+ul.appendChild(li);
+
 })
 
 //휴지통 버튼으로 localstorage 데이터 삭제
@@ -37,7 +42,7 @@ ul.addEventListener('click', (e) => {
     //localstorage에 저장된 정보를 splice를 하고 
     //다시 localstorage에 setItem으로 정보를 저장(splice된 정보)
     //그러고 화면에 다시 뿌려줘야 실시간 업데이트가 보여짐
-    ul.innerHTML='';
+    ul.innerHTML = '';
     cart.forEach((product, index) => {
       console.log(product);
       const li = document.createElement('li');
@@ -64,7 +69,6 @@ ul.addEventListener('click', (e) => {
     })
   }
 })
-
 
 
 
